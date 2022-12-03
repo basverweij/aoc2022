cc4315f9b94c4b0e6d6c8f824ae1f0aac0b5a9dc
Bas Verweij <bas@basverweij.nl>
2022-12-03 22:03:36 +0100

    feat: complete day 2, puzzle 2

--- src/AdventOfCode2022.Day02/Program.cs
+++ src/AdventOfCode2022.Day02/Program.cs
@@ -1,8 +1,32 @@
 ﻿var lines = await File.ReadAllLinesAsync("input.txt");
 
-var puzzle1 = lines.Select(ParseLine).Select(PlayRound).Sum();
+var puzzle1 = lines.Select(ParseLine1).Select(PlayRound).Sum();
 
 Console.WriteLine($"Day 2 - Puzzle 1: {puzzle1}");
+
+var puzzle2 = lines.Select(ParseLine2).Select(DetermineHand).Select(PlayRound).Sum();
+
+Console.WriteLine($"Day 2 - Puzzle 2: {puzzle2}");
+
+static (Hands, Hands) DetermineHand((Hands, Outcomes) round)
+{
+    var (opponent, outcome) = round;
+
+    var hand = (opponent, outcome) switch
+    {
+        (Hands.Rock, Outcomes.Lose) => Hands.Scissors,
+        (Hands.Paper, Outcomes.Lose) => Hands.Rock,
+        (Hands.Scissors, Outcomes.Lose) => Hands.Paper,
+
+        (Hands.Rock, Outcomes.Win) => Hands.Paper,
+        (Hands.Paper, Outcomes.Win) => Hands.Scissors,
+        (Hands.Scissors, Outcomes.Win) => Hands.Rock,
+
+        _ => opponent,
+    };
+
+    return (opponent, hand);
+}
 
 static int PlayRound((Hands, Hands) round)
 {
@@ -20,7 +44,7 @@ static int Outcome(Hands opponent, Hands player)
         (Hands.Paper, Hands.Scissors) => 6,
         (Hands.Scissors, Hands.Rock) => 6,
 
-        // loose
+        // lose
         (Hands.Rock, Hands.Scissors) => 0,
         (Hands.Paper, Hands.Rock) => 0,
         (Hands.Scissors, Hands.Paper) => 0,
@@ -30,14 +54,20 @@ static int Outcome(Hands opponent, Hands player)
     };
 }
 
-static (Hands, Hands) ParseLine(string line)
-{
-    return ((Hands)(line[0] - 'A'), (Hands)(line[2] - 'X'));
-}
+static (Hands, Hands) ParseLine1(string line) => ((Hands)(line[0] - 'A'), (Hands)(line[2] - 'X'));
+
+static (Hands, Outcomes) ParseLine2(string line) => ((Hands)(line[0] - 'A'), (Outcomes)(line[2] - 'X'));
 
 enum Hands
 {
     Rock,
     Paper,
     Scissors,
+}
+
+enum Outcomes
+{
+    Lose,
+    Draw,
+    Win,
 }

a2b5e22e46153e59934424adae86d63b4104b9c7
basverweij <basverweij@users.noreply.github.com>
2022-12-03 21:07:45 +0100

    feat: day 1 (#2)

---
+++ src/AdventOfCode2022.Day01/Program.cs
@@ -0,0 +1,29 @@
+﻿var lines = await File.ReadAllLinesAsync("input.txt");
+
+var elves = new List<int>();
+
+var currentCalories = 0;
+
+foreach (var line in lines)
+{
+    if (line == string.Empty)
+    {
+        elves.Add(currentCalories);
+
+        currentCalories = 0;
+
+        continue;
+    }
+
+    currentCalories += int.Parse(line);
+}
+
+elves.Sort();
+
+var day1 = elves.Last();
+
+Console.WriteLine($"Day 1 - Puzzle 1: {day1}");
+
+var day2 = elves.TakeLast(3).Sum();
+
+Console.WriteLine($"Day 1 - Puzzle 2: {day2}");

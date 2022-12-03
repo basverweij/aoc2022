920054e14b632d447cb59102c650f2de138940e6
Bas Verweij <bas@basverweij.nl>
2022-12-03 20:54:37 +0100

    feat: complete day 1, puzzle 1

---
+++ src/AdventOfCode2022.Day01/Program.cs
@@ -0,0 +1,24 @@
+﻿var lines = await File.ReadAllLinesAsync("input.txt");
+
+var maxCalories = 0;
+
+var currentCalories = 0;
+
+foreach (var line in lines)
+{
+    if (line == string.Empty)
+    {
+        if (currentCalories > maxCalories)
+        {
+            maxCalories = currentCalories;
+        }
+
+        currentCalories = 0;
+
+        continue;
+    }
+
+    currentCalories += int.Parse(line);
+}
+
+Console.WriteLine($"Day 1 - Puzzle 1: {maxCalories}");

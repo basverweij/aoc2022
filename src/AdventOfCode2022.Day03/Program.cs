4378949b8e4daab926804fc25f3584c915833ca9
Bas Verweij <bas@basverweij.nl>
2022-12-04 21:03:47 +0100

    feat: complete day 3, puzzle 1

---
+++ src/AdventOfCode2022.Day03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+var lines = await File.ReadAllLinesAsync("input.txt");
+
+var puzzle1 = lines.Select(GetDuplicate).Select(GetPriority).Sum();
+
+Console.WriteLine($"Day 3 - Puzzle 1: {puzzle1}");
+
+static char GetDuplicate(
+    string line) =>
+    line[..(line.Length / 2)].Intersect(line[(line.Length / 2)..]).Single();
+
+static int GetPriority(
+    char item) =>
+    item switch
+    {
+        >= 'a' and <= 'z' => item - 'a' + 1,
+        >= 'A' and <= 'Z' => item - 'A' + 27,
+        _ => 0,
+    };

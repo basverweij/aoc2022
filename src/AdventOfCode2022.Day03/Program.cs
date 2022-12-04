b36a6e2c445a4136de4ec7ec10ddb80ce934de33
Bas Verweij <bas@basverweij.nl>
2022-12-04 21:13:37 +0100

    feat: complete day 3, puzzle 2

--- src/AdventOfCode2022.Day03/Program.cs
+++ src/AdventOfCode2022.Day03/Program.cs
@@ -6,9 +6,17 @@ var puzzle1 = lines.Select(GetDuplicate).Select(GetPriority).Sum();
 
 Console.WriteLine($"Day 3 - Puzzle 1: {puzzle1}");
 
+var puzzle2 = lines.Chunk(3).Select(GetBadge).Select(GetPriority).Sum();
+
+Console.WriteLine($"Day 3 - Puzzle 2: {puzzle2}");
+
 static char GetDuplicate(
     string line) =>
     line[..(line.Length / 2)].Intersect(line[(line.Length / 2)..]).Single();
+
+static char GetBadge(
+    string[] lines) =>
+    lines[0].Intersect(lines[1]).Intersect(lines[2]).Single();
 
 static int GetPriority(
     char item) =>

04c732a63e7175415397d603b6eaf80206aa2886
basverweij <basverweij@users.noreply.github.com>
2022-12-04 21:14:03 +0100

    feat: day 3 (#6)

---
+++ src/AdventOfCode2022.Day03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+var lines = await File.ReadAllLinesAsync("input.txt");
+
+var puzzle1 = lines.Select(GetDuplicate).Select(GetPriority).Sum();
+
+Console.WriteLine($"Day 3 - Puzzle 1: {puzzle1}");
+
+var puzzle2 = lines.Chunk(3).Select(GetBadge).Select(GetPriority).Sum();
+
+Console.WriteLine($"Day 3 - Puzzle 2: {puzzle2}");
+
+static char GetDuplicate(
+    string line) =>
+    line[..(line.Length / 2)].Intersect(line[(line.Length / 2)..]).Single();
+
+static char GetBadge(
+    string[] lines) =>
+    lines[0].Intersect(lines[1]).Intersect(lines[2]).Single();
+
+static int GetPriority(
+    char item) =>
+    item switch
+    {
+        >= 'a' and <= 'z' => item - 'a' + 1,
+        >= 'A' and <= 'Z' => item - 'A' + 27,
+        _ => 0,
+    };

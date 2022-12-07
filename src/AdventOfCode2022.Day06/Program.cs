05301e956ea38f3d54c3281926294e3ff13013c8
Bas Verweij <bas@basverweij.nl>
2022-12-07 21:16:46 +0100

    feat: complete day 6, puzzle 2

--- src/AdventOfCode2022.Day06/Program.cs
+++ src/AdventOfCode2022.Day06/Program.cs
@@ -14,6 +14,20 @@ for (var i = 4; i < input.Length; i++)
 
 Console.WriteLine($"Day 6 - Puzzle 1: {puzzle1}");
 
+var puzzle2 = -1;
+
+for (var i = 14; i < input.Length; i++)
+{
+    if (AreAllDifferent(input[(i - 14)..i]))
+    {
+        puzzle2 = i;
+
+        break;
+    }
+}
+
+Console.WriteLine($"Day 6 - Puzzle 2: {puzzle2}");
+
 static bool AreAllDifferent(
     ReadOnlySpan<char> chars)
 {

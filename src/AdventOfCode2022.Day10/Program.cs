0cd0477d975052411d67056d1720b65f6ff019de
Bas Verweij <bas@basverweij.nl>
2022-12-18 14:37:33 +0100

    feat: complete day 10, puzzle 2

--- src/AdventOfCode2022.Day10/Program.cs
+++ src/AdventOfCode2022.Day10/Program.cs
@@ -2,6 +2,8 @@
 
 var instructions = lines.Select(Parse).ToArray();
 
+var screen = new char[240];
+
 var i = -1;
 
 var signalStrengths = new List<int>();
@@ -27,6 +29,8 @@ for (var cycle = 1; ; cycle++)
         signalStrengths.Add(cycle * x);
     }
 
+    DrawPixel(cycle);
+
     if (--remainingCycles == 0)
     {
         x += instructions[i].dx;
@@ -37,6 +41,13 @@ var puzzle1 = signalStrengths.Sum();
 
 Console.WriteLine($"Day 10 - Puzzle 1: {puzzle1}");
 
+Console.WriteLine($"Day 10 - Puzzle 2:");
+
+for (i = 0; i < 6; i++)
+{
+    Console.WriteLine(screen[(i * 40)..(i * 40 + 40)]);
+}
+
 static (int cycles, int dx) Parse(string line)
     => line switch
     {
@@ -44,3 +55,16 @@ static (int cycles, int dx) Parse(string line)
         ['a', 'd', 'd', 'x', ' ', ..] => (2, int.Parse(line[5..])),
         _ => throw new ArgumentOutOfRangeException(nameof(line)),
     };
+
+void DrawPixel(
+    int cycle)
+{
+    var i = (cycle - 1) % 240; // position in screen buffer
+
+    var j = i % 40; // position in current row
+
+    screen[i] =
+        j >= (x - 1) && j <= (x + 1) ?
+        '#' :
+        '.';
+}

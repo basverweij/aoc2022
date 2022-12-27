825a90fc74954c85cd13e0c886b2e3c339126045
Bas Verweij <bas@basverweij.nl>
2022-12-27 21:21:58 +0100

    feat: complete day 12, puzzle 1

---
+++ src/AdventOfCode2022.Day12/Program.cs
@@ -0,0 +1,90 @@
+﻿var lines = File.ReadAllLines("input.txt");
+
+var map = lines.Select(line => line.ToCharArray()).ToArray();
+
+var sizeY = map.Length;
+
+var sizeX = map[0].Length;
+
+var start = (x: 0, y: 0);
+
+var end = (x: 0, y: 0);
+
+for (int y = 0; y < sizeY; y++)
+{
+    for (var x = 0; x < sizeX; x++)
+    {
+        if (map[y][x] == 'S')
+        {
+            start = (x, y);
+
+            map[y][x] = 'a';
+        }
+        else if (map[y][x] == 'E')
+        {
+            end = (x, y);
+
+            map[y][x] = 'z';
+        }
+    }
+}
+
+var steps = Enumerable.Range(0, sizeY).Select(_ => Enumerable.Repeat(int.MaxValue, sizeX).ToArray()).ToArray();
+
+steps[start.y][start.x] = 0;
+
+var candidates = new Queue<(int x, int y)>();
+
+candidates.Enqueue(start);
+
+while (candidates.TryDequeue(out var candidate))
+{
+    if (candidate == end)
+    {
+        break;
+    }
+
+    var (x, y) = candidate;
+
+    var s = steps[y][x];
+
+    if (x > 0 &&
+        (map[y][x - 1] - map[y][x]) <= 1 &&
+        steps[y][x - 1] > s + 1)
+    {
+        candidates.Enqueue((x - 1, y));
+
+        steps[y][x - 1] = s + 1;
+    }
+
+    if (x < sizeX - 1 &&
+        (map[y][x + 1] - map[y][x]) <= 1 &&
+        steps[y][x + 1] > s + 1)
+    {
+        candidates.Enqueue((x + 1, y));
+
+        steps[y][x + 1] = s + 1;
+    }
+
+    if (y > 0 &&
+        (map[y - 1][x] - map[y][x]) <= 1 &&
+        steps[y - 1][x] > s + 1)
+    {
+        candidates.Enqueue((x, y - 1));
+
+        steps[y - 1][x] = s + 1;
+    }
+
+    if (y < sizeY - 1 &&
+        (map[y + 1][x] - map[y][x]) <= 1 &&
+        steps[y + 1][x] > s + 1)
+    {
+        candidates.Enqueue((x, y + 1));
+
+        steps[y + 1][x] = s + 1;
+    }
+}
+
+var puzzle1 = steps[end.y][end.x];
+
+Console.WriteLine($"Day 12 - Puzzle 1: {puzzle1}");

bbcf767e5d03e5a3a61629e3c24c002fdeb2026d
Bas Verweij <bas@basverweij.nl>
2022-12-27 21:26:42 +0100

    refactor: extract local function

--- src/AdventOfCode2022.Day12/Program.cs
+++ src/AdventOfCode2022.Day12/Program.cs
@@ -29,62 +29,67 @@ for (int y = 0; y < sizeY; y++)
     }
 }
 
-var steps = Enumerable.Range(0, sizeY).Select(_ => Enumerable.Repeat(int.MaxValue, sizeX).ToArray()).ToArray();
-
-steps[start.y][start.x] = 0;
-
-var candidates = new Queue<(int x, int y)>();
-
-candidates.Enqueue(start);
-
-while (candidates.TryDequeue(out var candidate))
-{
-    if (candidate == end)
-    {
-        break;
-    }
-
-    var (x, y) = candidate;
-
-    var s = steps[y][x];
-
-    if (x > 0 &&
-        (map[y][x - 1] - map[y][x]) <= 1 &&
-        steps[y][x - 1] > s + 1)
-    {
-        candidates.Enqueue((x - 1, y));
-
-        steps[y][x - 1] = s + 1;
-    }
-
-    if (x < sizeX - 1 &&
-        (map[y][x + 1] - map[y][x]) <= 1 &&
-        steps[y][x + 1] > s + 1)
-    {
-        candidates.Enqueue((x + 1, y));
-
-        steps[y][x + 1] = s + 1;
-    }
-
-    if (y > 0 &&
-        (map[y - 1][x] - map[y][x]) <= 1 &&
-        steps[y - 1][x] > s + 1)
-    {
-        candidates.Enqueue((x, y - 1));
-
-        steps[y - 1][x] = s + 1;
-    }
-
-    if (y < sizeY - 1 &&
-        (map[y + 1][x] - map[y][x]) <= 1 &&
-        steps[y + 1][x] > s + 1)
-    {
-        candidates.Enqueue((x, y + 1));
-
-        steps[y + 1][x] = s + 1;
-    }
-}
-
-var puzzle1 = steps[end.y][end.x];
+int puzzle1 = FindPath(map, sizeY, sizeX, start, end);
 
 Console.WriteLine($"Day 12 - Puzzle 1: {puzzle1}");
+
+static int FindPath(char[][] map, int sizeY, int sizeX, (int x, int y) start, (int x, int y) end)
+{
+    var steps = Enumerable.Range(0, sizeY).Select(_ => Enumerable.Repeat(int.MaxValue, sizeX).ToArray()).ToArray();
+
+    steps[start.y][start.x] = 0;
+
+    var candidates = new Queue<(int x, int y)>();
+
+    candidates.Enqueue(start);
+
+    while (candidates.TryDequeue(out var candidate))
+    {
+        if (candidate == end)
+        {
+            break;
+        }
+
+        var (x, y) = candidate;
+
+        var s = steps[y][x];
+
+        if (x > 0 &&
+            (map[y][x - 1] - map[y][x]) <= 1 &&
+            steps[y][x - 1] > s + 1)
+        {
+            candidates.Enqueue((x - 1, y));
+
+            steps[y][x - 1] = s + 1;
+        }
+
+        if (x < sizeX - 1 &&
+            (map[y][x + 1] - map[y][x]) <= 1 &&
+            steps[y][x + 1] > s + 1)
+        {
+            candidates.Enqueue((x + 1, y));
+
+            steps[y][x + 1] = s + 1;
+        }
+
+        if (y > 0 &&
+            (map[y - 1][x] - map[y][x]) <= 1 &&
+            steps[y - 1][x] > s + 1)
+        {
+            candidates.Enqueue((x, y - 1));
+
+            steps[y - 1][x] = s + 1;
+        }
+
+        if (y < sizeY - 1 &&
+            (map[y + 1][x] - map[y][x]) <= 1 &&
+            steps[y + 1][x] > s + 1)
+        {
+            candidates.Enqueue((x, y + 1));
+
+            steps[y + 1][x] = s + 1;
+        }
+    }
+
+    return steps[end.y][end.x];
+}
